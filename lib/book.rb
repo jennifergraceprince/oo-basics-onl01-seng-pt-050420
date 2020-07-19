09d26309ed3fa44f7e6dcf33936c170440a89b53
Jennifer Prince <jennifergraceprince@gmail.com>
2020-07-19 03:51:08 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,17 +12,7 @@ class Book
     @title
   end
   
-  def author
-    @author
-  end 
   
-  def page_count
-    @page_count
-  end 
-  
-  def genre
-    @genre
-  end
  
   def turn_page
     puts "Flipping the page...wow, you read fast!"

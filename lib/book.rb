0bb7b4edd513b2dc0dff48ea928dd7687dc3a43a
Jennifer Prince <jennifergraceprince@gmail.com>
2020-07-19 03:50:43 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,27 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+#  attr_accessor :author, :page_count, :genre
  
   def initialize(title)
     @title = title
+    @author = author 
+    @page_count = page_acount 
+    @genre = genre
   end
  
   def title
     @title
+  end
+  
+  def author
+    @author
+  end 
+  
+  def page_count
+    @page_count
+  end 
+  
+  def genre
+    @genre
   end
  
   def turn_page
